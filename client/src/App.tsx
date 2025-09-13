63249d9418a61a27ed417c4ccc75ff7e3e684bd2
BYB Digital <buildyourbranddigital@gmail.com>
2025-09-13 20:12:11 +0000

    asdd

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,9 @@
-import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { Router as WouterRouter, Switch, Route } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
 
 // Import Components
 import Header from "@/components/Header";
