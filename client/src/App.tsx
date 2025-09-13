0b5711c0b7f0306f32ed7b48b8da79625c1f3aa6
BYB Digital <buildyourbranddigital@gmail.com>
2025-09-13 20:52:09 +0000

    asdfg

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -44,7 +44,6 @@ function App() {
               <AppRoutes />
             </main>
             <Footer />
-            <Hero />
             <ScrollToTop />
           </WouterRouter>
         </div>
